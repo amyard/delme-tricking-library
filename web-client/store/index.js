d57ddc27b1dbeb8669391cc2a1d72ec06d987dcd
amyard <maksymturenko@gmail.com>
2021-02-13 21:14:43 +0900

    tricks. create and diplay. front and back

---
+++ web-client/store/index.js
@@ -0,0 +1,31 @@
+﻿import axios from "axios";
+
+const initState = () => ({
+  message: "init"
+})
+
+export const state = initState;
+
+
+// mutations is sync method
+// actions is async methods
+
+export const mutations = {
+  setMessage(state, message) {
+    state.message = message;
+  },
+
+  reset(state) {
+    Object.assign(state, initState());
+  }
+}
+
+// nuxtServerInit - is working only once in SSR
+export const actions = {
+  async nuxtServerInit({commit, dispatch}) {
+    const message = (await axios.get("http://localhost:5000/api/home")).data;
+    console.log('message:  ', message);
+    commit("setMessage", message);
+    await dispatch('tricks/fetchTricks');
+  }
+}
---
+++ web-client/store/tricks.js
@@ -0,0 +1,35 @@
+﻿import axios from "axios";
+
+const initState = () => ({
+  tricks: []
+})
+
+export const state = initState;
+
+
+// mutations is sync method
+// actions is async methods
+
+export const mutations = {
+  setTricks(state, {tricks}) {
+    state.tricks = tricks;
+  },
+
+  reset(state) {
+    Object.assign(state, initState());
+  }
+}
+
+export const actions = {
+  async fetchTricks({commit}) {
+    const tricks = (await axios.get("http://localhost:5000/api/tricks")).data;
+    console.log('tricks:  ', tricks);
+    commit("setTricks", {tricks});
+  },
+
+  async createTricks({commit, dispatch}, {trick}) {
+    await axios.post("http://localhost:5000/api/tricks", trick);
+    await dispatch('fetchTricks');
+    console.log('tricks:  created');
+  }
+}
