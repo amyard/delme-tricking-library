2eda8f58e4dc026b38ab901449d18cd82fb7c46b
amyard <maksymturenko@gmail.com>
2021-02-14 17:47:40 +0900

    add steps to load video

--- TrickingLibrary.Api/Controllers/VideosController.cs
+++ TrickingLibrary.Api/Controllers/VideosController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace TrickingLibrary.Api.Controllers
 {
@@ -16,6 +17,15 @@ namespace TrickingLibrary.Api.Controllers
         {
             _env = env;
         }
+
+        [HttpGet("{video}")]
+        public IActionResult GetVideo(string video)
+        {
+            var mime = video.Split(".").Last();
+            var savePath = Path.Combine(_env.WebRootPath, video);
+            return new FileStreamResult(new FileStream(savePath, FileMode.Open, FileAccess.Read), "video/*");
+        }
+        
         [HttpPost]
         public async Task<IActionResult> UploadVideo(IFormFile video)
         {
@@ -28,7 +38,7 @@ namespace TrickingLibrary.Api.Controllers
                 await video.CopyToAsync(fileStream);
             }
             
-            return Ok();
+            return Ok(fileName);
         }
     }
 }
