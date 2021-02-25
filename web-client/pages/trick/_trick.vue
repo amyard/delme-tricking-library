8822b65d4a58a74e74c697bf41a7c4c21502d652
amyard <maksymturenko@gmail.com>
2021-02-26 00:35:34 +0900

    category page - UI

--- web-client/pages/trick/_trick.vue
+++ web-client/pages/trick/_trick.vue
@@ -3,7 +3,7 @@
     <div class="mx-2" v-if="submissions">
       <div v-for="x in 1">
         <div v-for="s in submissions">
-          {{ s.id }} - {{ s.description }} - {{ s.trickId }} - {{s.video}}
+          {{ s.id }} - {{ s.description }} - {{ s.trickId }} - {{ s.video }}
           <div>
             <video width="400" controls :src="`http://localhost:5000/api/videos/${s.video}`"></video>
           </div>
@@ -11,27 +11,26 @@
       </div>
     </div>
 
-    <div class="mx-2 sticky">
-      <v-sheet class="pa-3 mt-2">
-        <div class="text-h6">{{ trick.name  }}</div>
 
-        <v-divider class="my-1"></v-divider>
+    <v-sheet class="pa-3 ma-2 sticky">
+      <div class="text-h6">{{ trick.name }}</div>
 
-        <div class="text-body-2">{{ trick.description  }}</div>
-        <div class="text-body-2">{{ trick.difficulty  }}</div>
+      <v-divider class="my-1"></v-divider>
 
-        <v-divider class="my-1"></v-divider>
+      <div class="text-body-2">{{ trick.description }}</div>
+      <div class="text-body-2">{{ trick.difficulty }}</div>
 
-        <div v-for="rd in relatedData" v-if="rd.data.length > 0">
-          <div class="text-subtitle-1">{{ rd.title }}</div>
-          <v-chip-group>
-            <v-chip v-for="d in rd.data" :key="rd.idFactory(d)" small :to="rd.routeFactory(d)">
-              {{d.name}}
-            </v-chip>
-          </v-chip-group>
-        </div>
-      </v-sheet>
-    </div>
+      <v-divider class="my-1"></v-divider>
+
+      <div v-for="rd in relatedData" v-if="rd.data.length > 0">
+        <div class="text-subtitle-1">{{ rd.title }}</div>
+        <v-chip-group>
+          <v-chip v-for="d in rd.data" :key="rd.idFactory(d)" small :to="rd.routeFactory(d)">
+            {{ d.name }}
+          </v-chip>
+        </v-chip-group>
+      </div>
+    </v-sheet>
   </div>
 </template>
 
@@ -41,30 +40,30 @@ import {mapGetters, mapState} from 'vuex';
 export default {
   computed: {
     ...mapState('submissions', ['submissions']),
-    ...mapState('tricks', ['categories','tricks']),
+    ...mapState('tricks', ['categories', 'tricks']),
     ...mapGetters('tricks', ['trickById']),
     trick() {
       return this.trickById(this.$route.params.trick)
     },
-    relatedData(){
+    relatedData() {
       return [
         {
-          title:"Categories",
+          title: "Categories",
           data: this.categories.filter(x => this.trick.categories.indexOf(x.id) >= 0),
           idFactory: c => `category-${c.id}`,
-          routeFactory: c => `/`,
+          routeFactory: c => `/category/${c.id}`,
         },
         {
-          title:"Prerequisites",
+          title: "Prerequisites",
           data: this.tricks.filter(x => this.trick.prerequisites.indexOf(x.id) >= 0),
           idFactory: t => `trick-${t.id}`,
-          routeFactory: t => `/tricks/${t.id}`,
+          routeFactory: t => `/trick/${t.id}`,
         },
         {
-          title:"Progressions",
+          title: "Progressions",
           data: this.tricks.filter(x => this.trick.progressions.indexOf(x.id) >= 0),
           idFactory: t => `trick-${t.id}`,
-          routeFactory: t => `/tricks/${t.id}`,
+          routeFactory: t => `/trick/${t.id}`,
         }
       ]
     },
@@ -77,17 +76,11 @@ export default {
     return {
       title: this.trick.name,
       meta: [
-        { hid: 'description', name: 'description', content: this.trick.description }
+        {hid: 'description', name: 'description', content: this.trick.description}
       ]
     }
   }
 }
 </script>
 
-<style scoped>
-.sticky {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 48px;
-}
-</style>
+<style scoped></style>
